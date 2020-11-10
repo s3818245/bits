ec047b857ae5110abfd79e3997d5247f0bdd8e01
s3802828 <56726405+s3802828@users.noreply.github.com>
2020-11-10 19:26:32 +0700

    Add files via upload

---
+++ side_navbar.php
@@ -0,0 +1,14 @@
+<div class="d-flex wrapper" id="sidenav">
+    <nav id="sidenav">
+        <div class="bg-light border-right" id="sidenav">
+            <div class="list-group list-group-flush">
+                <a href="#" class="list-group-item list-group-item-action bg-light">g1</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">g2</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">g3</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">g4</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">g5</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">g6</a>
+            </div>
+        </div>
+    </nav>
+</div>
