c1cb4397cd582815664a439273ee4299b32a05af
s3818245 <s3818245@rmit.edu.vn>
2020-11-12 22:55:44 +0700

    post layout stuffs

--- it_system_project/resources/templates/frontend/header.php
+++ it_system_project/resources/templates/frontend/header.php
@@ -23,6 +23,10 @@
     <!-- Other CSS File-->
     <link rel="stylesheet" href="../public/css/style.css">
 
+    <!-- Other JS File -->
+
+    <script src="../public/js/tools.js"></script>
+
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
