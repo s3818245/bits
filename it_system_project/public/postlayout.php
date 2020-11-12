6e001c039d6a8eb70d322fb012ed44e099ceaa00
s3818245 <s3818245@rmit.edu.vn>
2020-11-12 22:54:59 +0700

    post layout js

--- it_system_project/public/postlayout.php
+++ it_system_project/public/postlayout.php
@@ -1,3 +1,4 @@
+<?php require_once("../config.php")?>
 <?php include(TEMPLATES. DS ."header.php")?>
 
 <!-- Post with image -->
@@ -16,24 +17,22 @@
         </div>
 
         <div class = "p-4">
-            <img class="card-img-bottom" src="http://placehold.it/750x300" alt=post-image>
+            <img class="card-img-bottom" src="http://placehold.it/750x300" alt="post-image">
         </div>
-        
+
         <div class = "card-footer text-muted">
             
-            <i class="fa fa-thumbs-up hover-icon"> 0 </i>
+            <i class="fa fa-thumbs-up hover-icon" id="post-01-up" onclick="votePost(this.id);" value="0"> 0 </i>
 
             &nbsp;&nbsp;
 
-            <i class=" fa fa-thumbs-down hover-icon "> 0 </i>
+            <i class="fa fa-thumbs-down hover-icon" id="post-01-down" onclick="votePost(this.id);" value="0"> 0 </i>
 
             &nbsp;&nbsp;&nbsp;&nbsp;
 
-            <i class=" far fa-comment-dots hover-icon "></i>
+            <i class="far fa-comment-dots hover-icon"></i>
 
             &nbsp;&nbsp;&nbsp;&nbsp;
-
-
         </div>
     </div>
 </div>
@@ -56,11 +55,11 @@
 
         <div class = "card-footer text-muted">
             
-            <i class="fa fa-thumbs-up hover-icon"> 0 </i>
+            <i class="fa fa-thumbs-up hover-icon" id="post-02-up" onclick="votePost(this.id);" value="0"> 0 </i>
 
             &nbsp;&nbsp;
 
-            <i class="fa fa-thumbs-down hover-icon"> 0 </i>
+            <i class="fa fa-thumbs-down hover-icon" id="post-02-down" onclick="votePost(this.id);" value="0"> 0 </i>
 
             &nbsp;&nbsp;&nbsp;&nbsp;
 
