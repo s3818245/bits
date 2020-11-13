fcac65a853f841dc64f014353f1b6f412fdd0c32
s3802828 <s3802828@rmit.edu.vn>
2020-11-13 23:36:42 +0700

    index little changes

--- it_system_project/public/index.php
+++ it_system_project/public/index.php
@@ -5,9 +5,8 @@
     
     <div class="col-sm-11">
         <!-- Search form  -->
-        <div class="row">
-            <div class="col-2"></div>
-            <div class="col-10 justify-content-center">
+        <div class="row" style="margin-top: 1%;">
+            <div class="col-10 justify-content-center" style="margin-left: auto; margin-right: auto">
                 <div class="form-inline d-flex justify-content-center md-form form-sm mt-0 mb-3">
                     <input class="form-control form-control-md" style="width: 80%;" type="text" placeholder="Search" aria-label="Search">
                 </div>
@@ -15,9 +14,8 @@
         </div>
 
         <!-- Main content -->
-        <div class="row">
-            <div class="col-sm-2"></div>         
-            <div class="col-sm-10">
+        <div class="row">        
+            <div class="col-sm-10" style="margin-left: auto; margin-right: auto; padding-bottom: 10%;">
                 <div class="row row-cols-lg-3 row-cols-md-2 row-cols-1">
                     <div class="col my-2">
                         <?php include("gamecard.php") ?>
@@ -31,19 +29,45 @@
                     <div class="col my-2">
                         <?php include("gamecard.php") ?>
                     </div>
+                    <div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div>
+                    <div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div>
+                    <div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div>
+                    <div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div>
+                    <div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div>
+                    <div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div>
+                    <div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div><div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div><div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div><div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div><div class="col my-2">
+                        <?php include("gamecard.php") ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     </div>
-
-    <!-- Main Page content -->
         
-
-    <!-- Test layout -->
-    <?php include("postlayout.php") ?>
 </div>
 </body>
 
 <?php include("../resources/templates/frontend/footer.php") ?>
+
+
 </html>
