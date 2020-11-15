9b510b0c443e39da68e1197ac9655117738c10d2
s3818520 <s3818520@rmit.edu.vn>
2020-11-15 10:48:23 +0700

    login+signup style

--- it_system_project/public/login.php
+++ it_system_project/public/login.php
@@ -1,19 +1,90 @@
-<h2 class="text-center">LOGIN </h2>
-<div class="container-fluid d-flex justify-content-center">
-    <div class="col-sm-6 align-self-center d-flex justify-content-center">
-        <form class="form-signin" action="" method="post" name="loginForm">
-            <div class="form-group">
-                <label for="">Username:<input type="text" name="username" class="form-control" placeholder="Enter Username Here"></label>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>UnknowName</title>
+
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+
+    <!-- Bootstap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+    <!-- Other CSS File-->
+    <link rel="stylesheet" href="it_system_project/public/css/style.css">
+
+    <!-- Font CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+</head>
+
+<style>
+    body {
+        background-color: cornflowerblue;
+    }
+
+    .form-group input {
+        border-style: none;
+        width: 100%;
+        font-size: 16px;
+        border: none;
+        border-bottom: 2px solid #777;
+        outline: none;
+        padding: 10px;
+        padding-left: 30px;
+        box-sizing: border-box;
+    }
+
+    .form-group input:focus {
+        border-bottom: 2px solid #03a9f4;
+    }
+
+    .form-group .fa {
+        position: absolute;
+        margin-left: 8px;
+        margin-top: 12px;
+        font-size: 18px;
+    }
+</style>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4" style="background-color: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border-radius: 15px; margin-top: 150px;">
+            <h2 class="text-center">LOG IN</h2>
+            <hr>
+            <div class="container-fluid d-flex justify-content-center">
+                <div class="col-sm-12 d-flex justify-content-center">
+                    <form class="form" action="" method="post" name="loginForm">
+                        <div class="form-group">
+                            <!-- <label for="">Username:</label> -->
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <input type="text" name="username" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <!-- <label for="">Password:</label> -->
+                            <i class="fa fa-unlock-alt" aria-hidden="true"></i>
+                            <input type="password" name="password" placeholder="Password">
+                        </div>
+                        <div class="form-group d-flex justify-content-center">
+                            <button type="submit" name="login" style="width: 100%" class="btn btn-primary">Login</button>
+                        </div>
+                        <h6 class="text-center">
+                            <a href="signup.php">Create a new account</a>
+                        </h6>
+                    </form>
+
+                </div>
             </div>
-            <div class="form-group">
-                <label for="password">Password:<input type="password" name="password" class="form-control" placeholder="Enter Password Here"></label>
-            </div>
-            <div class="form-group">
-                <input type="submit" name="submit" class="btn btn-primary">
-            </div>
-        </form>
+        </div>
+        <div class="col-4"></div>
     </div>
 </div>
-<h6 class="text-center">
-    <a href="signup.php">Create a new account</a>
-</h6>
--- it_system_project/public/signup.php
+++ it_system_project/public/signup.php
@@ -1,41 +1,100 @@
-<h2 class="text-center">SIGN UP</h2>
-<div class="container-fluid d-flex justify-content-center">
-    <div class="col-sm-6 d-flex justify-content-center">
-        <form class="form" action="" method="post" name="signUpForm">
-            <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="">First Name:<input type="text" name="firstname" class="form-control" placeholder="Enter Firstname Here"></label>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>UnknowName</title>
+
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+
+    <!-- Bootstap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+    <!-- Other CSS File-->
+    <link rel="stylesheet" href="it_system_project/public/css/style.css">
+
+
+</head>
+
+<style>
+    body {
+        background-color: cornflowerblue;
+    }
+
+    .form-group input {
+        border-style: none;
+        width: 100%;
+        font-size: 16px;
+        border: none;
+        border-bottom: 2px solid #777;
+        outline: none;
+        padding: 10px;
+        box-sizing: border-box;
+    }
+
+    .form-group input:focus {
+        border-bottom: 2px solid #03a9f4;
+    }
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4" style="background-color: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border-radius: 15px; margin-top: 80px;">
+            <h2 class="text-center">SIGN UP</h2>
+            <hr>
+            <div class="container-fluid d-flex justify-content-center">
+                <div class="col-sm-12 d-flex justify-content-center">
+                    <form class="form" action="" method="post" name="signUpForm">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <!-- <label for="">First Name:</label> -->
+                                <input type="text" name="firstname" placeholder="First name">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <!-- <label for="">Last Name:</label> -->
+                                <input type="text" name="lastname" placeholder="Last name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <!-- <label for="">Username:</label> -->
+                            <input type="text" name="username" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <!-- <label for="">Password:</label> -->
+                            <input type="password" name="password" placeholder="Password">
+                        </div>
+                        <label for="">Gender:</label>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="male" id="male" value="maleOption" checked>
+                            <label class="form-check-label" for="male">
+                                Male
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="female" id="female" value="femaleoption">
+                            <label class="form-check-label" for="female">
+                                Female
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Date of Birth (MM/dd/yyyy):</label>
+                            <input type="date" name="dob" id="dob" placeholder="">
+                        </div>
+
+                        <div class="form-group d-flex justify-content-center">
+                            <input type="submit" name="submit" class="btn btn-primary" style="width: 100%">
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="form-group col-md-6">
-                <label for="">Last Name:<input type="text" name="lastname" class="form-control" placeholder="Enter Lastname Here"></label>
-            </div>
-            </div>
-            <div class="form-group">
-                <label for="">Username:<input type="text" name="username" class="form-control" placeholder="Enter Username Here"></label>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:<input type="password" name="password" class="form-control" placeholder="Enter Password Here"></label>
-            </div>
-            <label for="">Gender:</label>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="male" id="male" value="maleOption" checked>
-                <label class="form-check-label" for="male">
-                    Male
-                </label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="female" id="female" value="femaleoption">
-                <label class="form-check-label" for="female">
-                    Female
-                </label>
-            </div>
-            <div class="form-group">
-                <label for="">Date of Birth (MM/dd/yyyy):</label>
-                <input type="date" name="dob" id="dob">
-            </div>
-            <div class="form-group">
-                <input type="submit" name="submit" class="btn btn-primary">
-            </div>
-        </form>
+        </div>
+        <div class="col-4"></div>
     </div>
 </div>
