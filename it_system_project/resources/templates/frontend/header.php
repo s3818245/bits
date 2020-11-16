135ac861ea22e2311c396d5b75f985bbe9fc916a
s3818245 <s3818245@rmit.edu.vn>
2020-11-16 22:02:48 +0700

    add icon

--- it_system_project/resources/templates/frontend/header.php
+++ it_system_project/resources/templates/frontend/header.php
@@ -31,6 +31,7 @@
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
 
 
 
