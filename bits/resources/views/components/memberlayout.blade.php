354dbf96f7ca917cb10a6aaa782cf2c31d0706f1
s3802828 <s3802828@rmit.edu.vn>
2020-11-24 01:14:50 +0700

    git again

--- bits/resources/views/components/memberlayout.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="px-4 pt-3">
-    <div class="px-2 pb-2" style="width: auto; display: flex;">
-        <img class="rounded-circle border border-logo" style="max-height: 70px; max-width: 70px;" src="{{url('/upload_image/gamecardlogo.png')}}" alt="Logo">
-        <h4 class="pl-2" style="margin-top: 20px;">User Name</h4>
-    </div>
-</div>
