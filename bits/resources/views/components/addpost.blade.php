4078a08d6b31f2635176f457077489fe7a09f13c
s3818245 <s3818245@rmit.edu.vn>
2020-11-27 18:20:20 +0700

    add modal for edit post

--- bits/resources/views/components/addpost.blade.php
+++ bits/resources/views/components/addpost.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="edit-image-post" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+<div class="modal fade" id="add-image-post" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editPost">Edit Post</h5>
+                <h5 class="modal-title" id="editPost">Post an Image</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
@@ -12,7 +12,7 @@
                 
                 <div class="modal-body">
                     <div class="form-row">
-                        <label for="post-title">Title</label>
+                        <label for="post-title"> Title</label>
                         <input class="form-control" type="text"></input>
                     </div>
 
@@ -22,14 +22,14 @@
                     </div>
 
                     <div class="form-row">
-                        <label for="post-image">Image</label>
+                        <label for="post-image">Select Image</label>
 
                         <input class="form-control-file" type="file">
                     </div>
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary">Post</button>
                 </div>
 
             </form>
@@ -37,11 +37,11 @@
     </div>
 </div>
 
-<div class="modal fade" id="edit-text-post" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+<div class="modal fade" id="add-text-post" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editPost">Edit Post</h5>
+                <h5 class="modal-title" id="editPost">Post a Discussion</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
@@ -52,17 +52,17 @@
                 <div class="modal-body">
                     <div class="form-row">
                         <label for="post-title">Title</label>
-                        <input class="form-control" type="text"></input>
+                        <input class="form-control" type="text" placeholder="Add post's title here"></input>
                     </div>
 
                     <div class="form-row">
                         <label for="post-decsription">Discussion</label>
-                        <input class="form-control" type="text"></input>
+                        <input class="form-control" type="text" placeholder="Add discussion here"></input>
                     </div>
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary">Post</button>
                 </div>
 
             </form>
--- bits/resources/views/components/postcard.blade.php
+++ bits/resources/views/components/postcard.blade.php
@@ -4,16 +4,16 @@
             <div class="card mb-4">
                 <div class="card-body text-muted">
                     <div class="row ml-2">
-                        <h5>What news?</h5>
+                        <h5>Choose something to post</h5>
                     </div>
                     <div class="row d-flex justify-content-between">
                         <div class="col-1">
                             <img class="rounded-circle border border-logo" style="max-height: 50px; max-width: 50px;" src="{{url('/upload_image/gamecardlogo.png')}}" alt="Logo">
                         </div>
-                        <div class="col-10 border rounded">
-                            <div class="mx-2 py-3" role="button" aria-placeholder="Post here">
-                                Post Here
-                            </div>
+                        <div class="col-10">
+                            <button class="btn btn-primary" data-toggle="modal" data-target="#add-image-post"> Image </button>
+                            <button class="btn btn-primary" data-toggle="modal" data-target="#add-text-post"> Discussion </button>
+                            @include('components.addpost')
                         </div>
                     </div>
                 </div>
