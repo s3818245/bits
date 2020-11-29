47adbda6a7bed4f6188579ba48d98a9e26d937fb
s3818245 <s3818245@rmit.edu.vn>
2020-11-29 22:40:45 +0700

    change avatar modal

--- bits/resources/views/profilepage.blade.php
+++ bits/resources/views/profilepage.blade.php
@@ -7,7 +7,16 @@
             <div class="col-sm-10 mr-auto ml-auto">
                 <div class="container mt-3">
                     <div id="profile_part">
-                        <img src="{{url('/upload_image/avatar')}}" alt="Profile pic" style="display: inline-block;">
+                        <div class="img-container">
+                            <img src="{{url('/upload_image/avatar')}}" alt="Profile pic"
+                                style="display: inline-block; object-fit: cover; height:210px; width:210px">
+                            <div class="img-button">
+                                <button id="userId-avatar" class="btn btn-light hover-button" value="avatar"
+                                    data-toggle="modal" data-target="#editImageModal" onclick="editImageModal(this.id)">
+                                    <ion-icon name="camera-outline" class="" style="font-size:30px"></ion-icon>
+                                </button>
+                            </div>
+                        </div>
                         <h2 style="display: inline-block; padding-left: 3%">Name (Username/Nickname)</h2>
                     </div>
                     <br>
@@ -28,31 +37,31 @@
                     <div class="tab-content">
                         <div id="about_me" class="container tab-pane active"><br>
                             <h3>About Me</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.</p>
                         </div>
                         <div id="joined_game" class="container tab-pane fade"><br>
                             <h3>Joined Game</h3>
                             <div class="container">
-                            @for($i = 0; $i < 3; $i++)
-                                <div class="row mb-4">
-                                @for($j = 0; $j < 3; $j++)
-                                    <div class="col-sm">
+                                @for($i = 0; $i < 3; $i++) <div class="row mb-4">
+                                    @for($j = 0; $j < 3; $j++) <div class="col-sm">
                                         @include('components.gamecard')
-                                    </div>
-                                @endfor
-                                </div>
-                            @endfor
                             </div>
+                            @endfor
                         </div>
-                        <div id="setting" class="container tab-pane fade"><br>
-                            <h3>Setting</h3>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                        </div>
+                        @endfor
                     </div>
                 </div>
+                <div id="setting" class="container tab-pane fade"><br>
+                    <h3>Setting</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                        totam rem aperiam.</p>
+                </div>
             </div>
-            @include('templates.footer')
         </div>
+    </div>
+    @include('templates.footer')
+    </div>
     </div>
 </body>
 
